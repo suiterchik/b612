53b999344118e739af800733c7b1ffd31be455fa
jinxiu.qi <suiterchik@gmail.com>
2017-02-08 00:48:06 +0800

    主页deme

--- webserver/main/base.py
+++ webserver/main/base.py
@@ -1,6 +1,10 @@
 from . import admin
+from flask import render_template
 
 
 @admin.route('/')
 def index():
-    return '<h1>hello b612<h1>'
+    notes = []
+    page = 1
+    max_page = 10
+    return render_template('index.html', notes=notes, page=page, max_page=max_page)

1835a3586b226a5431bc9ecf026bff200bd1c047
jinxiu.qi <suiterchik@gmail.com>
2017-02-14 00:19:14 +0800

    支持云盘

--- webserver/main/cloud.py
+++ webserver/main/cloud.py
@@ -1,18 +1,25 @@
+# -*- coding: utf-8 -*-
 from . import admin
 from flask import request, redirect, render_template
-from webserver.model import DBSession, Notes
 from flask.ext.login import login_required
+import os
+from datetime import datetime
 
 
 @admin.route('/cloud')
 def cloud():
-    article_list = []
-    return render_template('cloud.html', article_list=article_list)
+    filename_list = os.listdir('cloud')
+    filename_list.reverse()
+    print filename_list[0].decode('gbk').encode('utf-8')
+    files = [{'create_time': datetime.strptime(filename.split('@fuck_nvidia@')[0], '%Y-%m-%d %H-%M-%S').strftime('%Y-%m-%d %H:%M:%S'),
+              'name': filename.split('@fuck_nvidia@')[1]}
+             for filename in filename_list]
+    return render_template('cloud.html', files=files)
 
 
 @admin.route('/upload_file', methods=['POST'])
 @login_required
 def upload_file():
     f = request.files['file']
-    f.save('logs/' + f.filename)
+    f.save('cloud/' + datetime.now().strftime('%Y-%m-%d %H-%M-%S') + '@fuck_nvidia@' + f.filename)
     return redirect('/cloud')

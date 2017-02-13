2e7e9a636023308c00bb602f214aa57e20e29ca7
jinxiu.qi <suiterchik@gmail.com>
2017-02-14 01:00:24 +0800

    支持文件下载

--- webserver/main/cloud.py
+++ webserver/main/cloud.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import admin
-from flask import request, redirect, render_template
+from flask import request, redirect, render_template, send_from_directory, current_app, make_response, send_file
 from flask.ext.login import login_required
 import os
 from datetime import datetime
@@ -9,11 +9,13 @@ from datetime import datetime
 @admin.route('/cloud')
 @login_required
 def cloud():
+    sep = '@fuck_nvidia@'
     filename_list = os.listdir('cloud')
     filename_list.reverse()
-    files = [{'create_time': datetime.strptime(filename.split('@fuck_nvidia@')[0], '%Y-%m-%d %H-%M-%S').strftime('%Y-%m-%d %H:%M:%S'),
-              'name': filename.split('@fuck_nvidia@')[1]}
-             for filename in filename_list]
+    files = [{'name': filename.split(sep)[1],
+              'url': '/download_file?filename=' + filename,
+              'create_time': str(datetime.strptime(filename.split(sep)[0], '%Y-%m-%d %H-%M-%S')),
+              } for filename in filename_list]
     return render_template('cloud.html', files=files)
 
 
@@ -23,3 +25,13 @@ def upload_file():
     f = request.files['file']
     f.save('cloud/' + datetime.now().strftime('%Y-%m-%d %H-%M-%S') + '@fuck_nvidia@' + f.filename)
     return redirect('/cloud')
+
+
+@admin.route('/download_file')
+@login_required
+def download_file():
+    filename = request.args['filename']
+    path = '../cloud/'+filename
+    response = make_response(send_file(path))
+    response.headers["Content-Disposition"] = "attachment; filename=%s;" % filename.split('@fuck_nvidia@')[1]
+    return response
